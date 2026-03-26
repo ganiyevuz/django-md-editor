3b402d0e84dd6362394227c0be19ad2863edbf0d
Jakhongir <ganiyevuzb@gmail.com>
2026-03-26 23:57:34 +0500

    feat: add MarkdownEditorWidget with template and configurable options

---
+++ src/django_md_editor/widgets.py
@@ -0,0 +1,49 @@
+import json
+
+from django.forms.widgets import Textarea
+
+from django_md_editor.settings import get_setting
+
+
+class MarkdownEditorWidget(Textarea):
+    template_name = "django_md_editor/widget.html"
+
+    def __init__(
+        self,
+        toolbar=None,
+        preview_url="/md-editor/preview/",
+        upload_url="/md-editor/upload/",
+        height=None,
+        placeholder=None,
+        attrs=None,
+    ):
+        self.toolbar = toolbar
+        self.preview_url = preview_url
+        self.upload_url = upload_url
+        self.height = height
+        self.placeholder = placeholder
+        super().__init__(attrs=attrs)
+
+    def get_context(self, name, value, attrs):
+        context = super().get_context(name, value, attrs)
+        context["widget"]["toolbar"] = (
+            self.toolbar if self.toolbar is not None else get_setting("TOOLBAR")
+        )
+        context["widget"]["toolbar_json"] = json.dumps(context["widget"]["toolbar"])
+        context["widget"]["preview_url"] = self.preview_url
+        context["widget"]["upload_url"] = self.upload_url
+        context["widget"]["height"] = self.height or get_setting("DEFAULT_HEIGHT")
+        context["widget"]["placeholder"] = (
+            self.placeholder
+            if self.placeholder is not None
+            else get_setting("PLACEHOLDER")
+        )
+        context["widget"]["client_renderer"] = get_setting("CLIENT_RENDERER")
+        return context
+
+    class Media:
+        css = {"all": ("django_md_editor/css/editor.css",)}
+        js = (
+            "django_md_editor/js/marked.min.js",
+            "django_md_editor/js/editor.js",
+        )
---
+++ tests/test_widgets.py
@@ -0,0 +1,56 @@
+from django.test import TestCase
+
+from django_md_editor.widgets import MarkdownEditorWidget
+
+
+class MarkdownEditorWidgetTests(TestCase):
+    def test_default_attrs(self):
+        widget = MarkdownEditorWidget()
+        assert widget.template_name == "django_md_editor/widget.html"
+
+    def test_media_includes_css(self):
+        widget = MarkdownEditorWidget()
+        css = str(widget.media)
+        assert "django_md_editor/css/editor.css" in css
+
+    def test_media_includes_js(self):
+        widget = MarkdownEditorWidget()
+        js = str(widget.media)
+        assert "django_md_editor/js/editor.js" in js
+        assert "django_md_editor/js/marked.min.js" in js
+
+    def test_custom_toolbar(self):
+        toolbar = ["bold", "italic"]
+        widget = MarkdownEditorWidget(toolbar=toolbar)
+        ctx = widget.get_context("content", "test", {})
+        assert ctx["widget"]["toolbar"] == ["bold", "italic"]
+
+    def test_custom_height(self):
+        widget = MarkdownEditorWidget(height="500px")
+        ctx = widget.get_context("content", "test", {})
+        assert ctx["widget"]["height"] == "500px"
+
+    def test_default_height_from_settings(self):
+        widget = MarkdownEditorWidget()
+        ctx = widget.get_context("content", "test", {})
+        assert ctx["widget"]["height"] == "300px"
+
+    def test_custom_preview_url(self):
+        widget = MarkdownEditorWidget(preview_url="/custom/preview/")
+        ctx = widget.get_context("content", "test", {})
+        assert ctx["widget"]["preview_url"] == "/custom/preview/"
+
+    def test_custom_upload_url(self):
+        widget = MarkdownEditorWidget(upload_url="/custom/upload/")
+        ctx = widget.get_context("content", "test", {})
+        assert ctx["widget"]["upload_url"] == "/custom/upload/"
+
+    def test_default_placeholder(self):
+        widget = MarkdownEditorWidget()
+        ctx = widget.get_context("content", "", {})
+        assert ctx["widget"]["placeholder"] == "Add your comment here..."
+
+    def test_renders_without_error(self):
+        widget = MarkdownEditorWidget()
+        html = widget.render("content", "**hello**", {})
+        assert "md-editor" in html
