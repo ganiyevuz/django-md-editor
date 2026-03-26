55c85eb56e14d18a3c707824771efe707ea4dbe4
Jakhongir <ganiyevuzb@gmail.com>
2026-03-27 00:22:39 +0500

    feat: add convenience imports to package init

--- src/django_md_editor/__init__.py
+++ src/django_md_editor/__init__.py
@@ -1,2 +1,17 @@
 __version__ = "0.1.0"
 default_app_config = "django_md_editor.apps.DjangoMdEditorConfig"
+
+from django_md_editor.renderers import BaseRenderer, DefaultRenderer  # noqa: E402
+from django_md_editor.uploads import (  # noqa: E402
+    BaseUploadHandler,
+    DefaultUploadHandler,
+)
+from django_md_editor.widgets import MarkdownEditorWidget  # noqa: E402
+
+__all__ = [
+    "MarkdownEditorWidget",
+    "BaseRenderer",
+    "DefaultRenderer",
+    "BaseUploadHandler",
+    "DefaultUploadHandler",
+]
